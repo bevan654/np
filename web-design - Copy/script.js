3564876195c3c7d7b82a6355d10629ab759158c1
Bevan <71739185+bevan654@users.noreply.github.com>
2022-06-13 12:40:41 +0930

    Add files via upload

--- web-design - Copy/script.js
+++ web-design - Copy/script.js
@@ -4,13 +4,27 @@ $(document).ready(()=> {
 
     document.addEventListener('scroll', () => {
         if(document.documentElement.scrollTop > 0){
-            $('.navigation').css('background-color','white');
-            $('.navigation').css('color','black');
+            $('nav').css('background-color','white');
+            $('nav a').css('color','black');
+
+            $('nav').attr('class','nav')
+
+
+//hi bevan shaquan
+
 
             
         }else if(document.documentElement.scrollTop == 0){
-            $('.navigation').css('background-color','transparent');
-            $('.navigation').css('color','white');
+            $('nav').css('background-color','transparent');
+            $('nav a').css('color','white');
+
+            $('nav').attr('class','navigation')
+
+
+
+    
+
+            
 
 
         }
@@ -21,3 +35,9 @@ $(document).ready(()=> {
     })
 
 })
+
+
+
+let redirect = () => {
+    document.location = 'index.html#section2Title'
+}
